af61a652745a8f063d4770499e9ca3746a0f8c13
santoshpavan <santoshpavan.psp@gmail.com>
2020-06-26 16:08:53 -0400

    getting command line inputs

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
 const chalk = require('chalk');
-const ans = chalk.red.inverse('Hello ') + chalk.blue.underline.bold('World');
-console.log(ans);
+const validator = require('validator');
+
+const error = chalk.bold.red;
+const success = chalk.bold.green;
+
+const command = process.argv[2];
+
+switch(command){
+    case 'add':
+        console.log(success('Note added!'));
+        break;
+
+    case 'remove':
+        console.log(success('Note removed!'));
+        break;
+    
+    default:
+        console.log(error("Please enter correct command"));
+}
